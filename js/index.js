6a68970a0efc43cc30e2edf9a4e347d7e030bbc3
Numonovdev <ebxuzsher@gmail.com>
2024-06-11 03:22:25 +0300

    finished

---
+++ js/index.js
@@ -0,0 +1,173 @@
+// integer9
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       alert(uch)
+// } else {alert('xato son kiritdingiz eror')}
+
+// integer10
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = ikki / 10
+//       alert(uch)
+//       alert(Math.trunc(ikki))
+     //  console.log(uch);
+     //  console.log(Math.trunc(ikki))
+// } else {alert('xato son kiritdingiz eror')}
+// birinchi birliklarni chiqaryabdi keyin entrni bossa 10liklarni chiqaryabdi alertda
+
+// integer11
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// let bir;
+// let y;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = Math.trunc(ikki / 10)
+//       bir = Math.trunc(a / 100)
+//       y = bir + ikki + uch;
+//       alert(y)
+// } else {alert('xato son kiritdingiz eror')}
+
+// integer12
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// let bir;
+// let y;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = Math.trunc(ikki / 10)
+//       bir = Math.trunc(a / 100)
+//       y = bir  + ikki * 10 + uch * 100;
+//       alert(y)
+// } else {alert('xato son kiritdingiz eror')}
+
+
+// integer13
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// let bir;
+// let y;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = Math.trunc(ikki / 10)
+//       bir = Math.trunc(a / 100)
+//       y = bir  + ikki * 100 + uch * 10;
+//       alert(y)
+// } else {alert('xato son kiritdingiz eror')}
+
+// integer14
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// let bir;
+// let y;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = Math.trunc(ikki / 10)
+//       bir = Math.trunc(a / 100)
+//       y = bir * 10 + ikki + uch * 100;
+//       alert(y)
+// } else {alert('xato son kiritdingiz eror')}
+
+
+// integer15
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// let bir;
+// let y;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = Math.trunc(ikki / 10)
+//       bir = Math.trunc(a / 100)
+//       y = bir * 10 + ikki * 100 + uch;
+//       alert(y)
+// } else {alert('xato son kiritdingiz eror')}
+
+
+// integer16
+
+// let a = prompt('faqat 3xonali son kiriting')
+// let uch;
+// let ikki;
+// let bir;
+// let y;
+// if(a > 99 && a <= 999){
+//       uch = a % 10;
+//       ikki = a % 100
+//       ikki = Math.trunc(ikki / 10)
+//       bir = Math.trunc(a / 100)
+//       y = bir * 100 + ikki + uch * 10;
+//       alert(y)
+// } else {alert('xato son kiritdingiz eror')}
+
+
+//  integer 17 va 18 ni tushunmadim.
+
+// integer 19
+
+// let n = prompt('nechi sekund o`tganini kiriting')
+// let min;
+// min = Math.trunc(n / 60)
+// alert(min)
+
+// integer 20
+
+// let n = prompt('nechi sekund o`tganini kiriting')
+// let soat;
+// soat = Math.trunc(n / 3600)
+// alert(soat)
+
+
+// integer 21
+
+// let n = prompt('nechi sekund o`tganini kiriting')
+// let min;
+// let soat;
+// min = n % 3600;
+// min = Math.trunc(min / 60);
+// soat = Math.trunc(n / 3600);
+// console.log('minut ',min ,'soat',soat)
+
+// integer 22
+
+// let n = prompt('nechi sekund o`tganini kiriting')
+// let sekund;
+// let soat;
+// sekund = n % 3600;
+// soat = Math.trunc(n / 3600);
+// console.log('sekund-',sekund ,'  soat-',soat)
+
+// integer 23
+
+// let n = prompt('nechi sekund o`tganini kiriting')
+// let sekund;
+// let min;
+// let soat;
+// sekund = n % 3600;
+// sekund = sekund % 60;
+// min = n % 3600;
+// min = Math.trunc(min / 60);
+// soat = Math.trunc(n / 3600);
+// console.log('sekund-',sekund, '  minut ',min ,'soat',soat)
